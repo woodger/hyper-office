33d80b2c320802b7ab67f6afd38291e728566396
Stanislav Potemkin <woodger@ya.ru>
2019-11-03 14:14:35 +0300

    Добавьте файлы проекта.

---
+++ App/Router.cs
@@ -0,0 +1,9 @@
+﻿using Nancy;
+
+namespace HyperOffice.App {
+  public class Router : NancyModule {
+    public Router() {
+      Get("/", args => "Hello World, it's Nancy on .NET Core");
+    }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Nancy.Hosting.Self;
+using System;
+using System.Configuration;
+
+namespace HyperOffice {
+  class Program {
+    static void Main(string[] args) {
+      string address = ConfigurationManager.AppSettings.Get("address");
+      string port = ConfigurationManager.AppSettings.Get("port");
+
+      Uri uri = new Uri("http://" + address + ":" + port);
+
+      NancyHost server = new NancyHost(uri);
+      server.Start();
+
+      Console.WriteLine("Server started on http://" + address + ":" + port);
+      Console.WriteLine("Press enter to exit the application");
+      Console.ReadLine();
+    }
+  }
+}
